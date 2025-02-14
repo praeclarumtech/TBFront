3782d85c8e67012ae451c3efc57e2c7111f3f8ca
vinit-praeclarum <vinit.praeclarum@gmail.com>
2025-02-14 19:23:20 +0530

    Delete TBAPI-development/TBAPI-development directory

--- TBAPI-development/TBAPI-development/src/helpers/generateApplicationNo.js
+++
@@ -1,8 +0,0 @@
-import Applicant from '../models/applicantModel.js';
-
-export const generateApplicantNo = async () => {
-  const lastApplicant = await Applicant.findOne().sort({ applicationNo: -1 });
-  return lastApplicant ? lastApplicant.applicationNo + 1 : 1001;
-};
-
-// module.exports = generateApplicantNo;
