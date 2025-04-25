48866fe39c30a4cd2894a9c3391373604763c75c
vinit-praeclarum <vinit.praeclarum@gmail.com>
2025-04-25 17:52:34 +0530

    config fixed

--- src/config.ts
+++ src/config.ts
@@ -8,16 +8,16 @@ interface Config {
  
 const config: Config = {
   api: {
-    // API_URL: "https://tbapi-jtu7.onrender.com/api/",
+    API_URL: "https://tbapi-jtu7.onrender.com/api/",
     // API_URL: "http://192.168.1.16:3000/api/",
-    API_URL: "http://192.168.1.3:3000/api/",
+    // API_URL: "http://192.168.1.3:3000/api/",
   },
 };
 export const configImage: Config = {
   api: {
-    // API_URL: "https://tbapi-jtu7.onrender.com/api/",
+    API_URL: "https://tbapi-jtu7.onrender.com/api/",
     // API_URL: "http://192.168.1.16:3000/api/",
-    API_URL: "http://192.168.1.3:3000/api/",
+    // API_URL: "http://192.168.1.3:3000/api/",
   },
 };
  
