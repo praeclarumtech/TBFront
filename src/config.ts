c7493b21653edd88cd13d0ec4777cb31c2cff0c2
vinit-praeclarum <vinit.praeclarum@gmail.com>
2025-07-25 15:16:22 +0530

    config fixed

--- src/config.ts
+++ src/config.ts
@@ -8,15 +8,15 @@ interface Config {
 
 const config: Config = {
   api: {
-    // API_URL: "https://tbapi-jtu7.onrender.com/api/",
-    API_URL: "http://192.168.1.16:3000/api/",
+    API_URL: "https://tbapi-jtu7.onrender.com/api/",
+    // API_URL: "http://192.168.1.16:3000/api/",
     // API_URL: "http://192.168.1.14:3000/api/",
   },
 };
 export const configImage: Config = {
   api: {
-    // API_URL: "https://tbapi-jtu7.onrender.com/api/",
-    API_URL: "http://192.168.1.16:3000/api/",
+    API_URL: "https://tbapi-jtu7.onrender.com/api/",
+    // API_URL: "http://192.168.1.16:3000/api/",
     // API_URL: "http://192.168.1.14:3000/api/",
   },
 };
