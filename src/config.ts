4cbaac2429d62902422a9186577f6aa93227622a
RaviPraeclarum <ravi@praeclarumtech.com>
2025-08-22 14:45:27 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -11,7 +11,7 @@ const config: Config = {
     // Production
     // API_URL: "https://tbapi-jtu7.onrender.com/api/",
     // staging
-       API_URL: https://tb-staging.onrender.com/api/,
+       API_URL: "https://tb-staging.onrender.com/api/"
     // API_URL: "http://192.168.1.16:3000/api/",
     // API_URL: "http://192.168.1.7:3000/api/",
   },
@@ -21,7 +21,7 @@ export const configImage: Config = {
     // Production
     // API_URL: "https://tbapi-jtu7.onrender.com/api/",
      // staging
-       API_URL: https://tb-staging.onrender.com,
+       API_URL: "https://tb-staging.onrender.com"
     // API_URL: "http://192.168.1.16:3000/api/",
     // API_URL: "http://192.168.1.7:3000/api/",
   },
