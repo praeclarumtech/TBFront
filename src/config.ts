e5e72c4f81b811f9bd8059d2ea47f3d8c7d1befe
RaviPraeclarum <ravi@praeclarumtech.com>
2025-09-08 17:36:48 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -8,14 +8,14 @@ interface Config {
 
 const config: Config = {
   api: {
-    API_URL: "https://tbapi-jtu7.onrender.com/api/",
+    API_URL: "https://tb-staging.onrender.com/api/",
     // API_URL: "http://localhost:3000/api/",
     // API_URL: "http://192.168.1.11:3000/api/",
   },
 };
 export const configImage: Config = {
   api: {
-    API_URL: "https://tbapi-jtu7.onrender.com/api/",
+    API_URL: "https://tb-staging.onrender.com/api/",
     // API_URL: "http://localhost:3000/api/",
     // API_URL: "http://192.168.1.11:3000/api/",
   },
