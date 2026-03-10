b47434f6dce9087e01c7822565e5822de301adf2
sonalRathod24 <sonal@praeclarumtech.com>
2026-03-10 17:35:34 +0530

    remove unused css

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -45,7 +45,7 @@ const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
         <div className="navbar-vertical navbar" style={{ zIndex: 10 }}>
           <Sidebar showMenu={showMenu} toggleMenu={ToggleMenu} />
         </div>
-        <div id="page-content" className="overflow-auto hide-scrollbar">
+        <div id="page-content" className="overflow-auto">
           <div
             className="header fixed-top bg-dark border-bottom-dark"
             style={{ ...contentShiftStyle, zIndex: 10 }}
@@ -54,7 +54,7 @@ const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
           </div>
 
           <div
-            className="content hide-scrollbar"
+            className="content"
             style={{
               // ...contentShiftStyle,
               marginTop: "66px",
